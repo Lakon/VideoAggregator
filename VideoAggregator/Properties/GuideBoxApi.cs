9582044083752ae0d3cfd9c9ab0ba0d5f1f65745
HendrickM9410 <Theanykey21@Macintosh.local>
2016-03-28 16:26:33 -0500

    Base Class
    Needs Review

---
+++ VideoAggregator/Properties/GuideBoxApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Json.Net;
+
+namespace VideoAggregator
+{
+	public class GuideBoxApiClass
+	{
+		string apiHome = "http://api-public.guidebox.com/v1.43/1/";
+		string apiKey = "rK4vt5WeKQvXmZgr8I0v7jbOVGCt9wtm/all/";
+
+
+
+		public string getShowID(string showID)
+		{
+			int limit1 = 0;
+			int limit2 = 0;
+
+			showID = apiHome + apiKey + limit1.ToString () + "/" + limit2.ToString ();
+			return showID;
+
+		}
+
+	}
+}
